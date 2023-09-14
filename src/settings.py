a4e99453727a934f4d45233224729644b399bd74
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-09-14 21:23:32 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Panoramic Dental X-rays"
 PROJECT_NAME_FULL: str = "Panoramic Dental X-rays With Segmented Mandibles"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
